1f48cd2161eb20ab891d612f4b4c70a8a2930dfe
DorinaFerencsik <36485244+DorinaFerencsik@users.noreply.github.com>
2020-10-13 22:58:53 +0200

    feat: Highlight.js, Material, FlexLayout, CodeBlock component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,50 @@
-import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserModule } from '@angular/platform-browser';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HighlightModule, HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    CoreModule,
+    HttpClientModule,
+    HighlightModule,
+    FlexLayoutModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        coreLibraryLoader: () => import('highlight.js/lib/core'),
+        lineNumbersLoader: () => import('highlightjs-line-numbers.js'),
+        languages: {
+          python: () => import('highlight.js/lib/languages/python'),
+        },
+      },
+    },
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,55 @@
+import { ClipboardModule } from '@angular/cdk/clipboard';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
+import { TranslateModule } from '@ngx-translate/core';
+import { HighlightModule, HIGHLIGHT_OPTIONS,  } from 'ngx-highlightjs';
+
+import { CodeBlockComponent } from './components/code-block/code-block.component';
+
+const components = [
+  CodeBlockComponent,
+];
+
+const vendorModules = [
+  ClipboardModule,
+  FlexLayoutModule,
+  HighlightModule,
+  MatButtonModule,
+  MatIconModule,
+  MatInputModule,
+  MatMenuModule,
+  ReactiveFormsModule,
+  TranslateModule,
+];
+
+@NgModule({
+  declarations: [components],
+  imports: [
+    CommonModule,
+    ...vendorModules,
+  ],
+  providers: [
+    {
+      provide: HIGHLIGHT_OPTIONS,
+      useValue: {
+        coreLibraryLoader: () => import('highlight.js/lib/core'),
+        lineNumbersLoader: () => import('highlightjs-line-numbers.js'),
+        languages: {
+          python: () => import('highlight.js/lib/languages/python'),
+        },
+      },
+    },
+  ],
+  exports: [
+    ...components,
+    CommonModule,
+    ...vendorModules,
+  ],
+})
+export class SharedModule { }
